459f744a5ce67ed0e0160d8e56a5ee7798412a10
Manuel Avelar <manuel@pixelcreart.net>
2016-04-18 10:54:16 -0600

    Done with tihs test

--- AllMyBooksArePacked/core/Home_controller.php
+++ AllMyBooksArePacked/core/Home_controller.php
@@ -8,8 +8,6 @@
 		$output["box"] = array();
 		$counter = 1;
 		
-		header('Content-Type: application/json');
-		
 		foreach ($booksList as $book) {
 			$bookContent = array();
 			$bookContent["id"] = $counter;
@@ -20,18 +18,33 @@
 			$content = "";
 		
 			while(!feof($fp)) {
-				$content .= fgets($fp, 1024);
+				$line = fgets($fp, 1024);
+				$content .= $line;
+				
+				// AUTHOR
+				if(strstr($line,'field-author')) {
+					preg_match_all('/<a href=\"(.*|field\-author)\">(.*)<\/a>/',$line,$author,PREG_SET_ORDER);
+					$bookContent["content"]["author"] = $author[0][2];
+				}
 			}
 			
 			fclose($fp);
 			
-			// POUNDS
 			preg_match_all("/Weight:<\/b> ([0-9][.][0-9] \w+)/", $content, $pounds, PREG_SET_ORDER);
 			$bookContent["totalWeight"] = $pounds[0][1];
+			$bookContent["content"]["shipping_weight"] = $pounds[0][1];
 			
 			// PRICES
 			preg_match_all("/([$][0-9]*[,]*[.][0-9]{2})/", $content, $prices, PREG_SET_ORDER);
 			$bookContent["content"]["price"] = $prices[0][0]." USD";
+			
+			// BOOK TITLE
+			preg_match_all('/<span id=\"btAsinTitle\"(.*?)span>/',$content,$title,PREG_SET_ORDER);		
+			$bookContent["content"]["title"] = trim(preg_replace('/(<([^>]+)>|>|<\/)/','',$title[0][1]));
+			
+			// ISBN-10
+			preg_match_all("/ISBN\-10:<\/b> (\d\w+)/", $content, $isbn10, PREG_SET_ORDER);
+			$bookContent["content"]["isbn-10"] = trim($isbn10[0][1]);
 			
 			array_push($output["box"],$bookContent);
 			
